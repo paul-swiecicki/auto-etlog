f9209b13de7417871cc40200146a95dd668e395b
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-08-16 10:02:48 +0200

    update dividing algorithm

--- src/helpers/getDividedAmounts.js
+++ src/helpers/getDividedAmounts.js
@@ -1,22 +1,63 @@
+const getQuotientAndRemainder = (dividend, divisor) => {
+  const quotient = Math.floor(dividend / divisor);
+  const remainder = dividend % divisor;
+  return { quotient, remainder };
+};
+
+const absoluteMaxMultiplier = 1.05;
+const splitHalfMaxMultiplier = 1.6;
+const doubleAbsMaxMultiplier = 2.11;
+
 const getDividedAmounts = (inputsValues) => {
   const dividedAmounts = [];
-  const amounts = inputsValues.amount.trim().split(" ");
+  let { maxAmount, amount } = inputsValues;
+  maxAmount = parseFloat(maxAmount);
+  const amounts = amount.trim().split(" ");
 
+  let prevAmount = null;
   for (let i = 0; i < amounts.length; i++) {
-    const curAmount = amounts[i];
+    const curAmount = parseFloat(amounts[i]);
     if (!curAmount) continue;
 
-    if (curAmount > inputsValues.maxAmount) {
-      const fullAmounts = Math.floor(curAmount / inputsValues.maxAmount);
-      const rest = curAmount % inputsValues.maxAmount;
+    console.log({ curAmount, maxAmount, x: curAmount > maxAmount });
+    if (curAmount > maxAmount) {
+      const absoluteMax = maxAmount * absoluteMaxMultiplier + 3;
+      console.log(curAmount, maxAmount, absoluteMax);
+      if (curAmount < absoluteMax) {
+        const { quotient, remainder } = getQuotientAndRemainder(curAmount, 2);
 
-      for (let j = 0; j < fullAmounts; j++) {
-        dividedAmounts.push(inputsValues.maxAmount);
+        dividedAmounts.push(quotient, quotient + remainder);
+      } else if (curAmount < maxAmount * splitHalfMaxMultiplier) {
+        const { quotient, remainder } = getQuotientAndRemainder(
+          curAmount + prevAmount,
+          2
+        );
+
+        dividedAmounts.push(quotient, quotient + remainder);
+      } else if (
+        curAmount > maxAmount * 2 &&
+        curAmount < maxAmount * doubleAbsMaxMultiplier
+      ) {
+        const { quotient, remainder } = getQuotientAndRemainder(
+          curAmount + prevAmount,
+          2
+        );
+
+        dividedAmounts.push(quotient, quotient + remainder);
+      } else {
+        const { quotient: fullAmounts, remainder: rest } =
+          getQuotientAndRemainder(curAmount, maxAmount);
+
+        for (let j = 0; j < fullAmounts; j++) {
+          dividedAmounts.push(maxAmount);
+        }
+        dividedAmounts.push(rest);
       }
-      dividedAmounts.push(rest);
     } else {
       dividedAmounts.push(curAmount);
     }
+
+    prevAmount = curAmount;
   }
 
   return dividedAmounts;

5037dbb1397bd61252e5f7b685988b201444ba97
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-08-23 22:58:51 +0200

    add replacing pages

--- src/atPrint/replaceAmount.js
+++ src/atPrint/replaceAmount.js
@@ -1,10 +1,20 @@
 const robot = require("robotjs");
 const { moveMouseRelToWindow } = require("../utils/moveMouseRelToWindow");
 
-const replaceAmount = (amount = 1, bounds) => {
-  console.log(amount);
-  moveMouseRelToWindow(300, 230, bounds);
+const defaultPages = 1;
+
+const replaceAmount = (amount = 1, pages = 1, bounds, isDateInput = false) => {
+  moveMouseRelToWindow(230, 130, bounds);
   robot.mouseClick();
+  if (pages !== defaultPages) {
+    robot.keyTap("a", "control");
+    robot.typeString(pages);
+  }
+  robot.keyTap("tab");
+  robot.keyTap("tab");
+
+  if (isDateInput) robot.keyTap("tab");
+
   robot.keyTap("a", "control");
   robot.typeString(amount);
 };

59458db3469987d85fd1898f5e4147ba39365493
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-08-26 15:25:22 +0200

    fix default pages bug

--- src/atPrint/replaceAmount.js
+++ src/atPrint/replaceAmount.js
@@ -1,15 +1,11 @@
 const robot = require("robotjs");
 const { moveMouseRelToWindow } = require("../utils/moveMouseRelToWindow");
 
-const defaultPages = 1;
-
 const replaceAmount = (amount = 1, pages = 1, bounds, isDateInput = false) => {
   moveMouseRelToWindow(230, 130, bounds);
   robot.mouseClick();
-  if (pages !== defaultPages) {
-    robot.keyTap("a", "control");
-    robot.typeString(pages);
-  }
+  robot.keyTap("a", "control");
+  robot.typeString(pages);
   robot.keyTap("tab");
   robot.keyTap("tab");
 

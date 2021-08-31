a0db351bd894cdb14186e812ab620520e415c5fc
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-08-31 15:25:46 +0200

    fix bugs and add error handling

--- src/helpers/sleepWhileLoadingWindowActive.js
+++ src/helpers/sleepWhileLoadingWindowActive.js
@@ -1,5 +1,10 @@
 const { getWindow } = require("../utils/getWindow");
 const { sleep } = require("../utils/sleep");
+const { showResultBox } = require("./manageResultBox");
+// const { showResultBox } = require("./helpers/manageResultBox");
+
+const windowCheckRate = 100;
+const maxWaitForWindow = 5000;
 
 const additionalStartWaitTime = 100;
 const additionalEndWaitTime = 100;
@@ -7,18 +12,33 @@ const sleepWhileLoadingWindowActive = (loadingWindowRegEx) =>
   new Promise(async (resolve, reject) => {
     await sleep(additionalStartWaitTime);
 
-    let canStopSleep = false;
+    let canStopSleep = false,
+      cantFindWindowTimeout;
     const interval = setInterval(async () => {
       const printingWindow = getWindow(loadingWindowRegEx);
 
-      if (canStopSleep && !printingWindow) {
+      if (printingWindow) {
+        canStopSleep = true;
+      } else if (canStopSleep) {
+        // } else {
         clearInterval(interval);
+        clearTimeout(cantFindWindowTimeout);
+
         await sleep(additionalEndWaitTime);
         resolve();
-      } else {
-        canStopSleep = true;
       }
-    }, 100);
+    }, windowCheckRate);
+
+    cantFindWindowTimeout = setTimeout(() => {
+      clearInterval(interval);
+
+      showResultBox({
+        msg: "Nie znaleziono okna drukowania",
+        desc: "Prawdopodobnie to problem z EtLogiem",
+        type: "error",
+      });
+      reject("Cant find loading window");
+    }, maxWaitForWindow);
   });
 
 module.exports = { sleepWhileLoadingWindowActive };

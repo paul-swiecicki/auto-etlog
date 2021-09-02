b8ea48a9301febcaa59c7dda1b8bff84f7ba609f
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-09-02 11:00:31 +0200

    move window size validation to auto

--- src/atProducts/clickPrintBtn.js
+++ src/atProducts/clickPrintBtn.js
@@ -2,8 +2,7 @@ const { moveMouseRelToWindow } = require("../utils/moveMouseRelToWindow");
 const robot = require("robotjs");
 const { sleep } = require("../utils/sleep");
 const atProducts = require("../atProducts");
-
-const windowTooSmallError = "window_too_small";
+const { showResultBox } = require("../helpers/manageResultBox");
 
 const clickPrintBtn = async (bounds, loadTime) => {
   /**
@@ -12,24 +11,13 @@ const clickPrintBtn = async (bounds, loadTime) => {
   const minWidthToMove = 970;
   let relToRight = true;
 
-  try {
-    if (bounds.width < 580) throw new Error(windowTooSmallError);
-    if (bounds.width < minWidthToMove) relToRight = false;
-    moveMouseRelToWindow(relToRight ? 390 : 570, 350, bounds, [
-      relToRight ? "right" : "left",
-      "bottom",
-    ]);
-    robot.mouseClick();
-    await sleep(loadTime);
-  } catch (err) {
-    if (err.message === atProducts.windowTooSmallError)
-      return showResultBox({
-        msg: 'Okno EtLog jest zbyt małe lub schowane, nie jest możliwe kliknięcie przycisku "drukuj".',
-        desc: "Powiększ okno i spróbuj ponownie.",
-        type: "error",
-      });
-    else throw err;
-  }
+  if (bounds.width < minWidthToMove) relToRight = false;
+  moveMouseRelToWindow(relToRight ? 390 : 570, 350, bounds, [
+    relToRight ? "right" : "left",
+    "bottom",
+  ]);
+  robot.mouseClick();
+  await sleep(loadTime);
 };
 
-module.exports = { clickPrintBtn, windowTooSmallError };
+module.exports = { clickPrintBtn };

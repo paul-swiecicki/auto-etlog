ee1f31c19a96ba633da646a7f6b69cb91687229f
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-09-02 11:26:34 +0200

    add file inputs to ui and fix name

--- src/elements/checkboxes.js
+++ src/elements/checkboxes.js
@@ -3,7 +3,7 @@ const { getElementsById } = require("../utils/getElementsById");
 const checkboxesIds = [
   "isDateInput",
   "doValidate",
-  "noLimitMaxAmount",
+  "isNoLimitMaxAmount",
   "isSingleAmounts",
   "dividedAmountsPeek",
 ];

f2f671f55b3847426660ce7fd09c4ab157da3bb9
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-09-06 12:17:25 +0200

    fix movemouserel default params

--- src/utils/moveMouseRelToWindow.js
+++ src/utils/moveMouseRelToWindow.js
@@ -8,12 +8,7 @@ const { getWindowRelativeCords } = require("./getWindowRelativeCords");
  * @param {object} bounds window bounds
  * @param {["top" | "left" | "bottom" | "right" ]} relTo default `["top", "left"]`
  */
-const moveMouseRelToWindow = (
-  x = 0,
-  y = 0,
-  bounds = { x: 0, y: 0, width: 500, height: 500 },
-  relTo = ["top", "left"]
-) => {
+const moveMouseRelToWindow = (x, y, bounds, relTo = ["top", "left"]) => {
   const [winX, winY] = getWindowRelativeCords(x, y, bounds, relTo);
   robot.moveMouse(winX, winY);
 };

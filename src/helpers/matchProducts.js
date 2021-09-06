329cd53ae48234a70b69076f950d2fa00f8730f8
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-09-06 12:12:10 +0200

    clean and add validation to matchProducts

--- src/helpers/matchProducts.js
+++ src/helpers/matchProducts.js
@@ -1,54 +1,40 @@
 const stringSimilarity = require("string-similarity");
+const { showResultBox } = require("./manageResultBox");
 
 const minRating = 0.2;
 
 const matchProducts = (products, order, headers) => {
-  // const productsArr = [];
-  // for (let j = 0; j < products.length; j++) {
-  //   const product = products[j].prod_name;
-
-  //   productsArr.push(product);
-  // }
   const preparedProducts = [];
 
   const productKey = headers.product;
   const productsArr = products.map((row) => row[productKey]);
-  console.log({ productsArr, products });
 
   for (let i = 0; i < order.length; i++) {
     const orderRow = order[i];
     const orderProduct = orderRow.product;
 
-    console.log({
-      orderProduct,
-      productsArr,
-    });
-    const similarityObj = stringSimilarity.findBestMatch(
-      orderProduct,
-      productsArr
-    );
+    let similarityObj;
+    try {
+      similarityObj = stringSimilarity.findBestMatch(orderProduct, productsArr);
+    } catch (error) {
+      console.log(error);
+      return showResultBox({
+        msg: "Coś poszło nie tak podczas wyszukiwania produktów",
+        desc: "Sprawdź, czy załadowane są odpowiednie pliki",
+        type: "error",
+      });
+    }
 
     if (similarityObj.bestMatch.rating < minRating) continue;
-    console.log(orderProduct, similarityObj.bestMatch, similarityObj);
     const matchedProduct = products[similarityObj.bestMatchIndex];
-    console.log(matchedProduct);
 
     preparedProducts.push({
       product: matchedProduct[headers.product],
       gtin: matchedProduct[headers.gtin],
       amounts: orderRow.amounts,
     });
-
-    // for (let j = 0; j < products.length; j++) {
-    //   const product = products[j].prod_name;
-
-    //   console.log({ product, orderProduct });
-    //   if (product === orderProduct) {
-    //     console.log(product);
-    //   }
-    // }
   }
-  console.log(preparedProducts);
+
   return preparedProducts;
 };
 
